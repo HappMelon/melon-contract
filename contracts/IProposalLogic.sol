6df44bbc2b4cae7211699761822b085b81428002
cxp-13 <lantianlaoli@gmail.com>
2024-04-10 11:56:52 +0800

    添加兑换积分功能；优化代币和提案合约逻辑；

--- contracts/IProposalLogic.sol
+++ contracts/IProposalLogic.sol
@@ -3,6 +3,113 @@ pragma solidity ^0.8.20;
 
 
 interface  IProposalLogic {
+    // 类型声明
+    // 提案
+    struct Proposal {
+        address proposer; // 提案发起人
+        string description; // 提案描述
+        uint256 stakeAmount; // 质押代币数量
+        bool active; // 提案是否活跃
+        bool isSettled; // 添加属性以跟踪提案是否已结算
+        bool isWagered;
+        uint256 endTime;
+    }
+    // 提议选项
+    struct Option {
+        string description; // 选项描述
+        uint256 voteCount; // 投票计数
+    }
+    // 质押
+    struct Stake {
+        uint256 amount; // 质押的金额
+        uint256 unlockTime; // 资金解锁的时间
+        address staker; // 质押者地址
+        bool isWagered; //是否对赌
+    }
+    // 投票记录
+    struct VoteRecord {
+        uint256 proposalId; // 提案ID
+        uint256 optionId; // 用户选择的选项ID
+        uint256 amount; // 投票数量
+    }
+    //事件
+    event Received(address caller, uint amount, string message);
+    event Deposited(address indexed user, uint amount);
+    event Withdrawn(address indexed user, uint amount);
+    event Voted(
+        address indexed _address,
+        uint256 indexed _proposalId,
+        uint256 indexed _optionId,
+        uint256 _amount
+    );
+    event ProposalAndOptionsSubmitted(
+        address indexed user,
+        uint256 indexed proposalIndex,
+        string proposalDescription,
+        string[] optionDescriptions,
+        uint256 endtime
+    );
+    event DepositForProposal(
+        address indexed staker,
+        uint256 amount,
+        bool staked,
+        uint256 unlockTime,
+        uint256 indexed stakeIndex
+    );
+    event TokensStaked(
+        address indexed user,
+        uint256 amount,
+        bool isForProposal
+    );
+    event FundsSettledForAverageQuality(
+        uint256 indexed proposalId,
+        address indexed proposer,
+        uint256 amountToReturn
+    );
+    event WithdrawalDetailed(
+        address indexed user,
+        uint256 amountWithdrawn,
+        uint256 balanceAfterWithdrawal
+    );
+    event UnlockTimeUpdated(
+        address indexed staker,
+        uint256 indexed stakeIndex,
+        uint256 newUnlockTime
+    );
+    event FundsPenalizedForNonCompliance(
+        uint256 indexed proposalId,
+        address indexed proposer,
+        uint256 penalty
+    );
+    event ProposalStatusChanged(uint256 proposalId, bool isActive);
+    event ProposalEndTime(uint256 _proposalId, uint256 endTime);
+    event ProposalForUser(
+        address indexed userAddress,
+        uint256 indexed proposalId,
+        string proposalDescription,
+        uint256 stakeAmount,
+        string[] optionDescriptions,
+        uint256 endtime
+    );
+    event StakeReleased(
+        address indexed user,
+        uint256 stakeIndex,
+        bool penalized,
+        uint256 amountReleased
+    );
+    event ProposalEnded(uint256 indexed proposalId, bool isActive);
+    event ProposalConcluded(uint256 indexed proposalId, bool isActive);
+    event RewardDistributed(
+        address indexed voter,
+        uint256 proposalId,
+        uint256 amount,
+        bool isWinner
+    );
+    event ExchangePoints(address indexed user, uint256 points);
+    // 错误
+
+    // 积分兑换
+    function exchangePoints(uint256 amount) external;
 
     function getUserBalance(address) external view returns (uint256);
 
